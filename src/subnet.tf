0e51040d6bf46e7a0eab962d4cbe4cf42e4320a0
chrisghill <chris@massdriver.cloud>
2022-06-11 19:50:59 -0600

    Create delegated subnet with the DB

---
+++ src/subnet.tf
@@ -0,0 +1,22 @@
+locals {
+  split_vnet_id = split("/", var.vnet.data.infrastructure.id)
+  vnet_name = element(local.split_vnet_id, length(local.split_vnet_id) - 1)
+  vnet_resource_group = element(local.split_vnet_id, index(local.split_vnet_id, "resourceGroups") + 1)
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = var.md_metadata.name_prefix
+  resource_group_name  = local.vnet_resource_group
+  virtual_network_name = local.vnet_name
+  address_prefixes     = [var.cidr]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
+  }
+}
