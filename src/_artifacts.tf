c0448076f3cfea193e8809c51e0f2eaba6e45148
chrisghill <chris@massdriver.cloud>
2022-06-22 14:04:31 -0600

    Remove 'type' field in artifact

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -14,7 +14,6 @@ locals {
 resource "massdriver_artifact" "authentication" {
   field                = "authentication"
   provider_resource_id = azurerm_postgresql_flexible_server.main.id
-  type                 = "postgresql-authentication"
   name                 = "PostgreSQL Server ${var.md_metadata.name_prefix} (${azurerm_postgresql_flexible_server.main.id})"
   artifact = jsonencode(
     {
