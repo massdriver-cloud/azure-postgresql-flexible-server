cae89092a06dd922d84652f9b37504b9220a9958
Michael Lacore <michael@massdriver.cloud>
2023-12-27 16:22:32 -0700

    Michael/#ORC-478/pgvector-support (#44)
    * Remove burstable compute skus
    
    * Add pgVector to addons
    
    * adding version update support
    
    * Refactoring version upgrade based on provider constraints
    
    * adding new versions of postgres

--- src/addons.tf
+++ src/addons.tf
@@ -4,3 +4,10 @@ resource "azurerm_postgresql_flexible_server_configuration" "pgbouncer" {
   server_id = azurerm_postgresql_flexible_server.main.id
   value     = "true"
 }
+
+resource "azurerm_postgresql_flexible_server_configuration" "pgvector" {
+  count     = var.addons.pgvector ? 1 : 0
+  name      = "azure.extensions"
+  server_id = azurerm_postgresql_flexible_server.main.id
+  value     = "VECTOR"
+}
