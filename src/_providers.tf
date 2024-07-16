8a367ed8920a8b35411f827b7c6e295812121e08
mclacore <mclacore@gmail.com>
2024-07-16 10:40:36 -0700

    Revert "Removing setting versions within providers file"
    This reverts commit 5a93842204c9552fe6f0267a35b713d02ef809ee.

--- src/_providers.tf
+++ src/_providers.tf
@@ -3,12 +3,15 @@ terraform {
   required_providers {
     massdriver = {
       source  = "massdriver-cloud/massdriver"
+      version = "~> 1.0"
     }
     utility = {
       source  = "massdriver-cloud/utility"
+      version = "~> 0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
